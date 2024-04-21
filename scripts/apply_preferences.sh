4e85803ce071350e1d26cfedc3e6cce198716d3d
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2024-04-21 14:50:13 +0200

    rename terminal profile backup and add commands to set it as default

--- scripts/apply_preferences.sh
+++ scripts/apply_preferences.sh
@@ -4,6 +4,10 @@ source config_base.sh
 # Apply saved preferences for MacOS
 apply_preferences preferences/macos
 
+# Change default profile in Terminal
+defaults write com.apple.Terminal "Default Window Settings" -string "Homebrew (p10k)"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Homebrew (p10k)"
+
 # Apply saved preferences for third-party apps
 apply_preferences preferences/third_party
 
